5199e8655ac3ea0cd312dad6f1706f2b1f95b446
doryan <doryanamt@gmail.com>
2023-02-02 15:48:54 +0100

    inicio django

---
+++ cosa/esports/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hola mundo')
+
+# queremos mostrar un listado de equipos, con su descripcion y los juegos asociados
+# en un url de equipo tiene que mostrarse /<nombre_equipo>/
+
+def detalle_equipo(request, nombre_equipo):
+    # e = Equipo.objects.get(nombre = nombre_equipo)
+    # juegos = ""
+
+    # for j in e.juego_set.all():
+    #     juegos += j.nombre
+    
+    Equipo.objects.all()
+    Equipo.objects.get(nombre="profes")
+    e = Equipo.objects.get(nombre="profes")
+
+    e.juego_set.all()
+
+    return HttpResponse(f'El nombre del equipo es {nombre_equipo}')
+
+def detalle_juego(request, nombre_juego):
+    return HttpResponse(f'El nombre del juego es es {nombre_juego}')
+
+def detalle_genero(request, nombre_genero):
+    return HttpResponse(f'El nombre del genero es {nombre_genero}')
+
+# en un url de juego /juego/<nombre_juego>
+# en un url de genero /genero/<nombre_generos>
